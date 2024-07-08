a31674e2c5649c9b4dff078ddacc0493d0e7fc5d
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-08 16:06:33 +0700

    test(capitalize): add test for undefined argument

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,8 +1,13 @@
 const capitalize = require('../src/capitalize');
 
-test('returns empty str', () => {
+
+test('returns empty string', () => {
   expect(capitalize('')).toBe('');
 });
+
+test('return empty string when undefined', () => {
+  expect(capitalize()).toBe('');
+})
 
 test('capitalizes single letter', () => {
   expect(capitalize('a')).toBe('A');
